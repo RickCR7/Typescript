e36ddf66da560306afa197590f030914ca9df74d
Akash Sen <iconicrick@gmail.com>
2025-07-31 10:59:41 +0530

    Tuples completed

---
+++ 02Basics/myTuples.js
@@ -0,0 +1,9 @@
+"use strict";
+// const user: (string | number)[] = [1,'as']
+Object.defineProperty(exports, "__esModule", { value: true });
+// tuple -> in a precise order
+var tuser;
+tuser = ["rg", 1223, true];
+var rgb = [255, 123, 112];
+var newUser = [112, "examapl@google.com"];
+newUser[1] = "hc.com";
---
+++ 02Basics/myTuples.ts
@@ -0,0 +1,16 @@
+// const user: (string | number)[] = [1,'as']
+
+// tuple -> in a precise order
+let tuser: [string, number, boolean]
+
+tuser = ["rg", 1223, true]
+
+let rgb: [number, number, number] = [255, 123, 112]
+
+type User = [number, string]
+
+const newUser : User = [112, "examapl@google.com"]
+
+newUser[1] = "hc.com"
+
+export{}
